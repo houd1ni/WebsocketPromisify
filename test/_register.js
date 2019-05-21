9c9850dd12d0744e38e1b0d09b0ca867b81bacb4
Michael <akiliev@outlook.com>
2019-05-21 14:17:43 +0300

    ci test stuff

---
+++ test/_register.js
@@ -0,0 +1,5 @@
+require("@babel/register")({
+  // This will override `node_modules` ignoring - you can alternatively pass
+  // an array of strings to be explicitly matched or a regex / glob
+  ignore: [],
+});
